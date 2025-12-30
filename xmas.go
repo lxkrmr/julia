266f69687eba02815adcaffffeb7fe924bac6f49
Alexander Kramer <alexander.kramer@timetoact.de>
2025-12-30 17:59:33 +0100

    :sparkles: add best question ever

---
+++ xmas.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var wahl string
+
+	for wahl != "A" {
+		fmt.Println("Willst du für immer mein sein?")
+		fmt.Println("A: Ja | B: Nein | C: Vielleicht")
+
+		fmt.Print("Deine Wahl: ")
+		fmt.Scanln(&wahl)
+
+		if strings.ToUpper(wahl) == "A" {
+			fmt.Println("Yay! 🥰")
+		} else {
+			fmt.Println("Ungültig, aber sowas von 🙂‍↔️😒😞")
+			fmt.Println("Probiere es noch einmal.")
+			fmt.Println("Tipp die einzig richtige Antwort ist A")
+			fmt.Println("")
+		}
+	}
+}
